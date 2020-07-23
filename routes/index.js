260d31ad7b93b589fabbe6f58fe8fce85ef7f42c
Brandon <brandon@mac.local>
2020-07-23 16:34:14 -0500

    Test commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Expresso' });
+  res.render('index', { title: 'Expressos' });
 });
 
 module.exports = router;
