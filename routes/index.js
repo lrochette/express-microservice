c3dd7d75b46be5c6f010f5ed970bde17d769e1ee
Brandon Phillips <brandon@codefresh.io>
2020-07-23 22:12:04 -0500

    Test 4

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Expressooo' });
+  res.render('index', { title: 'Expressoo' });
 });
 
 module.exports = router;
